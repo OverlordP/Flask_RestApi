bf138d3a493fb3accc633d12a5c3671aa9526a0f
OverlordP <piero.roca.g@uni.pe>
2022-09-14 21:27:15 -0500

    umm..

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ class DevelopmentConfig():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'eduardo32000'
-    MYSQL_DB = 'bibliotecas'
+    MYSQL_PASSWORD = 'secret'
+    MYSQL_DB = 'secret'
 
 
 config = {
